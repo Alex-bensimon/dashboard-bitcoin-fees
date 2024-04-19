b9ea6c52e003adb9bdb2adda0f178e0d8a70a337
Rayan Corbet <rayancorbet@outlook.fr>
2024-04-19 16:44:44 +0200

    BTC_get_fee

--- BTC_get_fee.py
+++ BTC_get_fee.py
@@ -1,9 +1,7 @@
-import requests
+import os
 import json
-import pandas as pd
-from datetime import datetime
-
 import requests
+from datetime import datetime
 
 def get_btc_fee():
     url = 'https://api.blockchain.info/mempool/fees'
@@ -11,13 +9,28 @@ def get_btc_fee():
         response = requests.get(url)
         if response.status_code == 200:
             data = response.json()
+            # Add the day's date to the data
+            today = datetime.now().strftime("%Y-%m-%d")
+            data['date'] = today
             return data
         else:
-            return f"Failed to fetch data: Status code {response.status_code}"
+            return None
     except Exception as e:
-        return f"An error occurred: {e}"
+        print(f"An error occurred: {e}")
+        return None
+
+def save_to_json(data):
+    # Check if JSON file for today's date already exists
+    today = datetime.now().strftime("%Y-%m-%d")
+    filename = f"btc_fee_{today}.json"
+    if not os.path.exists(filename):
+        with open(filename, 'w') as f:
+            json.dump(data, f)
+            print(f"Data saved to {filename}")
+    else:
+        print(f"Data for {today} already exists.")
 
 if __name__ == "__main__":
     btc_fee = get_btc_fee()
-    print(btc_fee)
-
+    if btc_fee:
+        save_to_json(btc_fee)
